74d9235fc8f5a3d76fa653b0ef044baf11214a11
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-15 22:20:13 +0800

    complete CarParkEscape

---
+++ CarParkEscape/Kata.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarParkEscape
+{
+    public class Kata
+    {
+        private int _currentLevel = 0;
+
+        public string[] escape(int[,] carpark)
+        {
+            // Code here
+            var totalLevels = carpark.GetLength(0);
+            var totalPosition = carpark.GetLength(1);
+            var temp = new int[totalLevels - 1];
+            int currentPosition = 0;
+            for (int level = 0; level < totalLevels; level++)
+            {
+                for (int position = 0; position < totalPosition; position++)
+                {
+                    if (carpark[level, position] == 1)
+                    {
+                        temp[level] = position;
+                    }
+
+                    if (carpark[level, position] == 2)
+                    {
+                        _currentLevel = level;
+                        currentPosition = position;
+                    }
+                }
+            }
+
+            var paths = new List<string>();
+            while (_currentLevel <= totalLevels - 1)
+            {
+                if (_currentLevel == totalLevels - 1)
+                {
+                    if (currentPosition == totalPosition - 1)
+                    {
+                        break;
+                    }
+                    paths.Add(GetLevelPath(currentPosition, totalPosition - 1));
+                }
+                else
+                {
+                    paths.Add(GetLevelPath(currentPosition, temp[_currentLevel]));
+                }
+
+                if (_currentLevel == temp.Length)
+                {
+                    break;
+                }
+                currentPosition = temp[_currentLevel];
+                paths.Add(GetDownPath(temp));
+            }
+
+            if (_currentLevel > totalLevels - 1)
+            {
+                paths.RemoveAt(paths.Count - 1);
+            }
+            return paths.ToArray();
+        }
+
+        private string GetDownPath(int[] temp)
+        {
+            var distance = 0;
+            while (_currentLevel < temp.Length - 1 && temp[_currentLevel] == temp[_currentLevel + 1])
+            {
+                distance++;
+                _currentLevel++;
+            }
+            _currentLevel++;
+            distance++;
+            return "D" + distance;
+        }
+
+        private string GetLevelPath(int start, int end)
+        {
+            var distance = start - end;
+            if (distance > 0)
+            {
+                return "L" + Math.Abs(distance);
+            }
+            else
+            {
+                return "R" + Math.Abs(distance);
+            }
+        }
+    }
+}
---
+++ CarParkEscapeTests/KataTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarParkEscape;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+
+namespace CarParkEscape.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod]
+        public void BasicTest1()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] expected = new string[] { "L4", "D1", "R4" };
+            var actual = kata.escape(carpark);
+            actual.ToExpectedObject().ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void BasicTest2()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 2, 0, 0, 1, 0 },
+                                          { 0, 0, 0, 1, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] expected = new string[] { "R3", "D2", "R1" };
+            var actual = kata.escape(carpark);
+            actual.ToExpectedObject().ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void BasicTest3()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 0, 2, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] expected = new string[] { "R3", "D3" };
+            var actual = kata.escape(carpark);
+            actual.ToExpectedObject().ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void BasicTest4()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 1, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] expected = new string[] { "L4", "D1", "R4", "D1", "L4", "D1", "R4" };
+            var actual = kata.escape(carpark);
+            actual.ToExpectedObject().ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void BasicTest5()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 0, 0, 0, 0, 2 } };
+            string[] expected = new string[] { };
+            var actual = kata.escape(carpark);
+            actual.ToExpectedObject().ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void BasicTest6()
+        {
+            Kata kata = new Kata();
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 0 },
+                                          { 1, 0, 0, 0, 2 },
+                                          { 1, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] expected = new string[] { "L4", "D2", "R4" };
+
+            var actual = kata.escape(carpark);
+            actual.ToExpectedObject().ShouldEqual(expected);
+        }
+    }
+}
