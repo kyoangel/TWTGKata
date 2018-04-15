a64cb484e30598bdc35622d8310089cbf00f1e2c
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-15 17:38:25 +0800

    add AnggramDetection

---
+++ AnagramDetection/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramDetection
+{
+    public class Kata
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            // your code goes here
+            return false;
+        }
+    }
+}
---
+++ AnagramDetectionTests/KataTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramDetection.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void IsAnagramTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
