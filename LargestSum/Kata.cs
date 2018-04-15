c92d6006f5b65602543a017dc09541d644f7339f
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-15 22:24:01 +0800

    initial LargetSum

---
+++ LargestSum/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestSum
+{
+    public class Kata
+    {
+        public static int LargestSum(int[] arr)
+        {
+            // your code to return the largest sum of any subsequence of arr
+            return 0;
+        }
+    }
+}
---
+++ LargestSumTests/KataTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LargestSum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestSum.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void LargestSumTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
