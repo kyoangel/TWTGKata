99e8220d9927dd382a9450c5a516eae86368b5f2
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-15 17:20:38 +0800

    Add project files.

---
+++ SortOdd/Kata.cs
@@ -0,0 +1,30 @@
+﻿namespace SortOdd
+{
+    public class Kata
+    {
+        public static int[] SortArray(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (IsEven(array, i))
+                    continue;
+                for (var j = 0; j < array.Length; j++)
+                {
+                    if (IsEven(array, j))
+                        continue;
+                    if (array[i] >= array[j]) continue;
+                    var temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                }
+            }
+
+            return array;
+        }
+
+        private static bool IsEven(int[] array, int i)
+        {
+            return array[i] % 2 == 0;
+        }
+    }
+}
---
+++ SortOddTests/KataTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortOdd.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void test_sample_1()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+        }
+
+        [TestMethod()]
+        public void test_sample_2()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, Kata.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+        }
+
+        [TestMethod()]
+        public void test_empty()
+        {
+            CollectionAssert.AreEqual(new int[] { }, Kata.SortArray(new int[] { }));
+        }
+
+        [TestMethod()]
+        public void test_all_even()
+        {
+            CollectionAssert.AreEqual(new int[] { 2, 4, 6, 8, 10 }, Kata.SortArray(new int[] { 2, 4, 6, 8, 10 }));
+        }
+    }
+}
