37806f314c2cf9b7b890ab6dfad31d6bf491dcc0
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-04-22 22:31:37 +0800

    complete

--- CountingChangeCombinations/Kata.cs
+++ CountingChangeCombinations/Kata.cs
@@ -1,16 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CountingChangeCombinations
+﻿namespace CountingChangeCombinations
 {
     public class Kata
     {
+        private static int[] _coins;
+
         public static int CountCombinations(int money, int[] coins)
         {
-            return 3;
+            _coins = coins;
+            var counts = FindCombination(money, 0);
+            return counts;
+        }
+
+        private static int FindCombination(int money, int index)
+        {
+            if (MatchAmount(money))
+            {
+                return 1;
+            }
+            else if (OverAmountOrNoNextCoin(money, index))
+            {
+                return 0;
+            }
+
+            var takeCurrent = FindCombination(money - _coins[index], index);
+            var takeNext = FindCombination(money, index + 1);
+            return takeCurrent + takeNext;
+        }
+
+        private static bool MatchAmount(int money)
+        {
+            return money == 0;
+        }
+
+        private static bool OverAmountOrNoNextCoin(int money, int index)
+        {
+            return money < 0 || index == _coins.Length;
         }
     }
 }
--- CountingChangeCombinationsTests/KataTests.cs
+++ CountingChangeCombinationsTests/KataTests.cs
@@ -11,5 +11,12 @@ namespace CountingChangeCombinations.Tests
             var actual = Kata.CountCombinations(4, new int[] { 1, 2 });
             Assert.AreEqual(3, actual);
         }
+
+        [TestMethod()]
+        public void test_sum_10_with_coin_5_2_3_should_be_4()
+        {
+            var actual = Kata.CountCombinations(10, new int[] { 5, 2, 3 });
+            Assert.AreEqual(4, actual);
+        }
     }
 }
