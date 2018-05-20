bcc5767f24a0b1aec52944a329f88918a5979327
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-20 22:57:03 +0800

    complete multies3Or5 kata

---
+++ Multies3Or5/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Multies3Or5
+{
+    public static class Kata
+    {
+        public static int Solution(int value)
+        {
+            // Magic Happens
+            var ints = new List<int>();
+            for (int i = 3; i < value; i++)
+            {
+                ints.Add(i);
+            }
+
+            return ints.Where(x => (x % 3 == 0) || (x % 5 == 0)).Sum();
+        }
+    }
+}
---
+++ Multies3Or5Tests/KataTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Multies3Or5.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void test_input_10_return_23()
+        {
+            var actual = Kata.Solution(10);
+            Assert.AreEqual(23, actual);
+        }
+    }
+}
