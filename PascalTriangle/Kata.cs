10aa2e1b86e13d1f6db80f98d9a39a3e379f87eb
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-20 21:12:23 +0800

    add PascalTriangle projects

---
+++ PascalTriangle/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PascalTriangle
+{
+    public static class Kata
+    {
+        public static List<int> PascalsTriangle(int n)
+        {
+            return new List<int>();
+        }
+    }
+}
