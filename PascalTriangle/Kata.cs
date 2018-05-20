43dba16d3458df227cc1a47aff2fda7ecaf9eb70
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-20 21:58:57 +0800

    complete PascalTriangle kata

--- PascalTriangle/Kata.cs
+++ PascalTriangle/Kata.cs
@@ -6,7 +6,37 @@ namespace PascalTriangle
     {
         public static List<int> PascalsTriangle(int n)
         {
-            return new List<int>();
+            if (n == 0)
+                return null;
+
+            return RecursiveGetPT(n);
+        }
+
+        private static List<int> RecursiveGetPT(int n)
+        {
+            if (n == 1)
+            {
+                return new List<int>() { 1 };
+            }
+            else
+            {
+                var list = RecursiveGetPT(n - 1);
+                var newList = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    if (i == 0 || i == n - 1)
+                    {
+                        newList.Add(1);
+                    }
+                    else
+                    {
+                        newList.Add(list[list.Count - n + i] + list[list.Count - n + i + 1]);
+                    }
+                }
+
+                list.AddRange(newList);
+                return list;
+            }
         }
     }
 }
---
+++ PascalTriangleTests/KataTests.cs
@@ -0,0 +1,45 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PascalTriangle.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void test_level_1()
+        {
+            var actual = Kata.PascalsTriangle(1);
+            actual.ToExpectedObject().ShouldEqual(new List<int>() { 1 });
+        }
+
+        [TestMethod()]
+        public void test_level_2()
+        {
+            var actual = Kata.PascalsTriangle(2);
+            actual.ToExpectedObject().ShouldEqual(new List<int>() { 1, 1, 1 });
+        }
+
+        [TestMethod()]
+        public void test_level_3()
+        {
+            var actual = Kata.PascalsTriangle(3);
+            actual.ToExpectedObject().ShouldEqual(new List<int>() { 1, 1, 1, 1, 2, 1 });
+        }
+
+        [TestMethod()]
+        public void test_level_4()
+        {
+            var actual = Kata.PascalsTriangle(4);
+            actual.ToExpectedObject().ShouldEqual(new List<int>() { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1 });
+        }
+
+        [TestMethod()]
+        public void test_level_5()
+        {
+            var actual = Kata.PascalsTriangle(5);
+            actual.ToExpectedObject().ShouldEqual(new List<int>() { 1, 1, 1, 1, 2, 1, 1, 3, 3, 1, 1, 4, 6, 4, 1 });
+        }
+    }
+}
