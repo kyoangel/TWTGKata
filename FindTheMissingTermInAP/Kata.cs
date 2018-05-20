0112fdb1388c65e745a41a72e8eaaa57ac928002
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-20 22:02:32 +0800

    create next kata project

---
+++ FindTheMissingTermInAP/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FindTheMissingTermInAP
+{
+    public static class Kata
+    {
+        public static int FindMissing(List<int> list)
+        {
+            return list[0];
+        }
+    }
+}
