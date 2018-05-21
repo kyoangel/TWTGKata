bd6e962b6d163cb06e4733bf2ae78b8a3a2a0bf2
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-05-21 22:32:08 +0800

    complete reverse word

---
+++ ReverseWord/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace ReverseWord
+{
+    public static class Kata
+    {
+        public static string ReverseWords(string str)
+        {
+            var array = str.Split(' ').Select(x => new string(x.Reverse().ToArray())).ToArray();
+            return string.Join(" ", array);
+        }
+    }
+}
---
+++ ReverseWordTests/KataTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReverseWord.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void ReverseWordsTest()
+        {
+            Assert.AreEqual("sihT si na !elpmaxe", Kata.ReverseWords("This is an example!"));
+        }
+    }
+}
