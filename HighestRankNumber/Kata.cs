2c40f211e4660dfb49e8c2c2df1ed82d8523ee03
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-06-10 20:18:04 +0800

    finish highest rank number

---
+++ HighestRankNumber/Kata.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace HighestRankNumber
+{
+    public class Kata
+    {
+        public static int HighestRank(int[] arr)
+        {
+            return arr.Any()
+                ? arr.GroupBy(x => x).OrderBy(g => g.Count()).ThenBy(c => c.Key).ToList().Last().Key
+                : 0;
+        }
+    }
+}
---
+++ HighestRankNumberTests/KataTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HighestRankNumber.Tests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void empty_array_should_return_0()
+        {
+            var actual = Kata.HighestRank(new int[0]);
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod()]
+        public void single_element_array_test()
+        {
+            var actual = Kata.HighestRank(new int[] { 6 });
+            Assert.AreEqual(6, actual);
+        }
+
+        [TestMethod()]
+        public void the_same_count_number_should_return_bigger_one()
+        {
+            var actual = Kata.HighestRank(new int[] { 6, 9 });
+            Assert.AreEqual(9, actual);
+        }
+
+        [TestMethod()]
+        public void the_most_frequent_number_should_be_return()
+        {
+            var actual = Kata.HighestRank(new int[] { 12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10 });
+            Assert.AreEqual(3, actual);
+        }
+    }
+}
